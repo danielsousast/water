9102616e1a82f80763245f1dbf92b231ff9812e5
Daniel Sousa <daniel.sousa@superlogica.com>
2020-10-10 15:22:19 -0300

    feat: add shared preferences

--- app/src/main/java/com/project/water/MainActivity.java
+++ app/src/main/java/com/project/water/MainActivity.java
@@ -3,6 +3,8 @@ package com.project.water;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Integer minute;
     private Integer interval;
     private boolean active = false;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
         timePicker = findViewById(R.id.timePicker);
 
         timePicker.setIs24HourView(true);
+
+        preferences = getSharedPreferences("waterdb", Context.MODE_PRIVATE);
+        active = preferences.getBoolean("active", false);
+
+        if(active){
+            button.setText(R.string.pause);
+
+            int interval = preferences.getInt("interval",0);
+            int hour = preferences.getInt("hour",timePicker.getCurrentHour());
+            int minute = preferences.getInt("minute",timePicker.getCurrentMinute());
+
+            editText.setText(String.valueOf(interval));
+            timePicker.setCurrentHour(hour);
+            timePicker.setCurrentMinute(minute);
+        }
     }
 
     public void notifyClick(View view) {
@@ -46,16 +64,30 @@ public class MainActivity extends AppCompatActivity {
         minute = timePicker.getCurrentMinute();
         interval = Integer.parseInt(intervalString);
 
+        SharedPreferences.Editor editor = preferences.edit();
+
         if(!active){
             button.setText(R.string.pause);
             Object color = ContextCompat.getColor(this, R.color.pink);
             //button.setBackground(color);
             active = true;
+
+            editor.putBoolean("active", true);
+            editor.putInt("interval", interval);
+            editor.putInt("hour", hour);
+            editor.putInt("minute", minute);
+            editor.apply();
         } else {
             button.setText(R.string.notify);
             int color = ContextCompat.getColor(this, R.color.colorAccent);
             //button.setBackground(color);
             active = false;
+
+            editor.putBoolean("active", false);
+            editor.remove("interval");
+            editor.remove("hour");
+            editor.remove("minute");
+            editor.apply();
         }
 
         Log.d("test", "Hour"+hour);
